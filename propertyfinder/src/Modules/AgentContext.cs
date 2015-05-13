391646770ca78e9384f28d40103ab1b5fb448195
frenix <efren.duranjr@gmail.com>
2015-05-13 20:53:18 +0800

    remove returning of password and authkey from select
    Signed-off-by: frenix <efren.duranjr@gmail.com>

--- propertyfinder/src/Modules/AgentContext.cs
+++ propertyfinder/src/Modules/AgentContext.cs
@@ -29,27 +29,27 @@ namespace OHWebService.Modules
         internal IList<AgentModel> Get(int top, int from, string filter)
 		{
 			// TODO: acknowledge parameter values.
-			String sql = "select * from property_agent order by AgentId";
+			String sql = "select AgentId, FirstName, LastName, EmailAddress, ProfileFileName, ProfileUrl, Description, ConfirmFlag from property_agent order by AgentId";
 			return CommonModule.GetDatabase().Query<AgentModel>(sql).ToList();
 		}
 		
         //maybe not use
         public AgentModel GetById(long id)
 		{
-        	String sql = "select * from property_agent where AgentId =" + id.ToString();
+        	String sql = "select AgentId, FirstName, LastName, EmailAddress, ProfileFileName, ProfileUrl, Description, ConfirmFlag from property_agent where AgentId =" + id.ToString();
 			return CommonModule.GetDatabase().FirstOrDefault<AgentModel>(sql);
 		}
         
 		public AgentModel GetByToken(string token)
 		{
-		    String sql = "select * from property_agent where AuthKey = @0";
+		    String sql = "select AgentId, FirstName, LastName, EmailAddress, ProfileFileName, ProfileUrl, Description, ConfirmFlag from property_agent where AuthKey = @0";
 			return CommonModule.GetDatabase().FirstOrDefault<AgentModel>(sql, token);
 		}
         
 		public AgentModel GetByEmailAddAndPwd(string  emailadd, string pswd)
 		{
 		    //String sql = "select * from property_agent where EmailAddress ='" + emailadd + "' and Password='" + pswd +"'";
-		    String sql = "select AgentId, FirstName, LastName, EmailAddress, Password, ConfirmFlag, ProfileFileName, ProfileUrl, Description  from property_agent where EmailAddress = @0 and Password=@1";
+		    String sql = "select AgentId, FirstName, LastName, EmailAddress, ProfileFileName, ProfileUrl, Description, ConfirmFlag from property_agent where EmailAddress = @0 and Password=@1";
 		    
 		    return CommonModule.GetDatabase().FirstOrDefault<AgentModel>(sql,emailadd,pswd);
 		}
@@ -57,7 +57,7 @@ namespace OHWebService.Modules
 		public AgentModel GetByEmailAdd(string  emailadd)
 		{
 		    //String sql = "select * from property_agent where EmailAddress ='" + emailadd + "' and Password='" + pswd +"'";
-		    String sql = "select * from property_agent where EmailAddress = @0";
+		    String sql = "select AgentId, FirstName, LastName, EmailAddress, ProfileFileName, ProfileUrl, Description, ConfirmFlag from property_agent where EmailAddress = @0";
 		    
 		    return CommonModule.GetDatabase().FirstOrDefault<AgentModel>(sql,emailadd);
 		}
